9115747d3223b3fdc1ca5bb8b7d5fdf64f1398e5
cuervo314 <gkswjddn30635@naver.com>
2025-02-24 00:24:19 +0900

    feat: UIManager 작성

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    GameUI gameUI;
+    PauseUI pauseUI;
+    GameOverUI gameOverUI;
+
+    private void Awake()
     {
-        
+        gameUI = FindFirstObjectByType<GameUI>();
+        pauseUI = FindFirstObjectByType<PauseUI>();
+        gameOverUI = FindFirstObjectByType<GameOverUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InGame()//게임 중
     {
-        
+        Time.timeScale = 1f;
+        gameUI.gameObject.SetActive(true);
+        pauseUI.gameObject.SetActive(false);
+        gameOverUI.gameObject.SetActive(false);
+    }
+
+    public void Pause()//일시정지
+    {
+        Time.timeScale = 0f;
+        pauseUI.gameObject.SetActive(true);
+    }
+    
+    public void GameOver()//게임 종료
+    {
+        Time.timeScale = 0f;
+        gameOverUI.gameObject.SetActive(true);
+        pauseUI.gameObject.SetActive(false);
     }
 }
