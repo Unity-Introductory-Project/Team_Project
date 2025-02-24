3f7952d9ee8de5de38b925586bf273c19ebd0f8e
Anvil925 <anvil_925@naver.com>
2025-02-24 16:09:14 +0900

    [Feat] 코인간격수정

--- Assets/Scripts/SYH/CoinSpawner.cs
+++ Assets/Scripts/SYH/CoinSpawner.cs
@@ -5,20 +5,20 @@ public class CoinSpawner : MonoBehaviour
 {
     public GameObject coinPrefab; // 생성할 코인 프리팹
     public Transform coinParent; // 코인을 저장할 부모 오브젝트
-    public Transform player; // 플레이어
-    public int maxCoinsPerGround = 0; // Ground당 최대 코인 개수
-    public float minY = 0f; // 코인 최소 Y 위치
-    public float maxY = 0f; // 코인 최대 Y 위치
-    public float coinSpacing = 0f; // 코인 간격
-    private List<GameObject> coinList = new List<GameObject>(); // 생성된 코인을 저장할 리스트
+    public int coinsPerGround = 3; // Ground당 생성할 코인 개수
+    public float coinSpacing = 1.5f; // 코인 간격 (일정하게 유지)
+    
+    private List<GameObject> coinList = new List<GameObject>(); // 생성된 코인 리스트
+
     public void SpawnCoins(Vector3 groundPosition, float groundWidth)
     {
-        int coinCount = Random.Range(1, maxCoinsPerGround + 1); // 랜덤한 개수의 코인 생성
-        for (int i = 0; i < coinCount; i++)
+        float startX = groundPosition.x - (groundWidth / 2) + coinSpacing / 2; // 코인 시작 위치
+        float yPos = groundPosition.y - 2.5f; // Ground 바로위에 생성되록 
+
+        for (int i = 0; i < coinsPerGround; i++)
         {
-            float xOffset = Random.Range(-groundWidth / 2, groundWidth / 2);
-            float yOffset = Random.Range(minY, maxY);
-            Vector3 coinPosition = new Vector3(groundPosition.x + xOffset, groundPosition.y + yOffset, 0);
+            float xPos = startX + (i * coinSpacing); // 일정한 간격으로 배치
+            Vector3 coinPosition = new Vector3(xPos, yPos, 0);
 
             GameObject coin = Instantiate(coinPrefab, coinPosition, Quaternion.identity, coinParent);
             coinList.Add(coin);
