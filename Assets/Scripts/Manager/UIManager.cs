94db3bf22f5d6c1e85a518947a1d57a99745db21
cuervo314 <gkswjddn30635@naver.com>
2025-02-24 21:18:38 +0900

    fix: 파일 정리
    초대형 패치

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    SoundManager soundManager;
+
+    GameUI gameUI;
+    PauseUI pauseUI;
+    GameOverUI gameOverUI;
+
+
+    //public TextMeshProUGUI OptionText;
+    //void Start()
+    private void Awake()
+    {
+        soundManager = FindFirstObjectByType<SoundManager>();
+
+        gameUI = FindFirstObjectByType<GameUI>();
+        pauseUI = FindObjectOfType<PauseUI>(true);
+        gameOverUI = FindObjectOfType<GameOverUI>(true);
+    }
+
+    public void InGame()//게임 중
+    {
+        Time.timeScale = 1f;
+        gameUI.gameObject.SetActive(true);
+        pauseUI.gameObject.SetActive(false);
+        gameOverUI.gameObject.SetActive(false);
+    }
+
+    public void Pause()//일시정지
+    {
+        Time.timeScale = 0f;
+        pauseUI.gameObject.SetActive(true);
+    }
+    
+    public void GameOver()//게임 종료
+    {
+        Time.timeScale = 0f;
+        gameOverUI.gameObject.SetActive(true);
+        pauseUI.gameObject.SetActive(false);
+    }
+
+    public void Restart()//게임 재시작
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void Resume()//게임 재개
+    {
+        InGame();
+    }
+}
