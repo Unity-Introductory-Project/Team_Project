f6e5d01ecb857a27fb4e480e571928cecc0c2c7a
Anvil925 <anvil_925@naver.com>
2025-02-24 13:06:43 +0900

    [Feat] GroundSpawner 수정

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -7,13 +7,17 @@ public class GroundSpawner : MonoBehaviour
     public Transform groundParent; // Ground를 저장할 Grid 오브젝트
     public int count = 0; // 처음 생성할 Ground 개수
     public float offsetX = 0f; // Ground X축 간격
+    public float Hole = 0f; // 밑으로 빠지는 구멍 만들기 위해 추가
     public Transform player; // 플레이어
     public List<GameObject> groundList = new List<GameObject>(); // 생성된 Ground 오브젝트를 저장할 리스트
 
     void Start()
     {
-        // 처음에 count만큼 Y축에 간격을 두고 Ground 생성
-        for (int i = 0; i < count; i++)
+        // 첫 번째 Ground는 Y = 0 고정
+        CreateGround(0f, true);
+
+        // 이후 Ground는 랜덤한 Y 위치로 생성
+        for (int i = 1; i < count; i++)
         {
             CreateGround(i * offsetX);
         }
@@ -24,14 +28,16 @@ public class GroundSpawner : MonoBehaviour
         if (player != null && groundList.Count > 0) // 플레이어가 존재하고 Ground가 1개 이상이라면
         {
             float lastGroundX = groundList[groundList.Count - 1].transform.position.x; // 마지막 Ground의 X위치
+            float lastGroundY = groundList[groundList.Count - 1].transform.position.y; // 마지막 Ground의 Y위치
             float firstGroundX = groundList[0].transform.position.x; // 첫 번째 Ground의 X위치
-            // Y축은 0, 2 중 랜덤으로 선택
-            offsetX = Random.Range(0, 2) * 2;
+            float firstGroundY = groundList[0].transform.position.y; // 첫 번째 Ground의 Y위치
+
+            Hole = Random.Range(3, 5);
 
             // 플레이어가 마지막 Ground의 X위치 - (offsetX의 1.5배) 이상 이동하면 새 Ground 생성
             if (player.position.x > lastGroundX - (offsetX * 0.8f)) 
             {
-                CreateGround(lastGroundX + offsetX);
+                CreateGround(lastGroundX + offsetX + Hole); // 마지막 Ground의 오른쪽에 새 Ground 생성
             }
 
             // 플레이어가 첫 번째 Ground의 X위치를 넘어서면 삭제
@@ -42,10 +48,12 @@ public class GroundSpawner : MonoBehaviour
         }
     }
 
-    void CreateGround(float xPos)
+    void CreateGround(float xPos, bool isFirstGround = false)
     {
-        GameObject ground = Instantiate(groundPrefab, new Vector3(xPos, 0, 0), Quaternion.identity, groundParent); // Ground 생성
-        groundList.Add(ground); // 생성한 Ground를 리스트에 추가
+        float yPos = isFirstGround ? 0f : new float[] { 0f, 2.5f, 5f }[Random.Range(0, 3)]; // 첫 번째 Ground는 Y=0 고정
+
+        GameObject ground = Instantiate(groundPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity, groundParent);
+        groundList.Add(ground);
     }
 
     void DestroyOldestGround()
