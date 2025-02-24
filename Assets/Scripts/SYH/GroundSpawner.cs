3ab545dd2a12d4603ab8220e2fd8649fc042586d
Anvil925 <anvil_925@naver.com>
2025-02-24 16:08:20 +0900

    [Feat] GroundSpawner 첫번째 그라운드에서 코인생성안되도록 수정

--- Assets/Scripts/SYH/GroundSpawner.cs
+++ Assets/Scripts/SYH/GroundSpawner.cs
@@ -11,6 +11,7 @@ public class GroundSpawner : MonoBehaviour
     public float Hole = 0f; // 밑으로 빠지는 구멍 만들기 위해 추가
     public Transform player; // 플레이어
     public List<GameObject> groundList = new List<GameObject>(); // 생성된 Ground 오브젝트를 저장할 리스트
+    private bool isFirstGround = true; // 첫 번째 Ground 여부 체크
 
     void Start()
     {
@@ -55,10 +56,12 @@ public class GroundSpawner : MonoBehaviour
 
         GameObject ground = Instantiate(groundPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity, groundParent);
         groundList.Add(ground);
-        if (coinSpawner != null)
+        if (!isFirstGround && coinSpawner != null)
         {
             coinSpawner.SpawnCoins(ground.transform.position, offsetX);
         }
+        
+        isFirstGround = false; // 첫 번째 Ground 생성 후 false로 변경
     }
 
     void DestroyOldestGround()
