086d8a361561265932deea1b76c561146111c488
Anvil925 <anvil_925@naver.com>
2025-02-24 14:46:51 +0900

    [Init] CoinSpawner

---
+++ Assets/Scripts/SYH/CoinSpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject coinPrefab; // 생성할 코인 프리팹
+    public Transform coinParent; // 코인을 저장할 부모 오브젝트
+    public Transform player; // 플레이어
+    public int maxCoinsPerGround = 3; // Ground당 최대 코인 개수
+    public float minY = 1f; // 코인 최소 Y 위치
+    public float maxY = 3f; // 코인 최대 Y 위치
+    public float coinSpacing = 2f; // 코인 간격
+
+    private List<GameObject> coinList = new List<GameObject>(); // 생성된 코인을 저장할 리스트
+
+    public void SpawnCoins(Vector3 groundPosition, float groundWidth)
+    {
+        int coinCount = Random.Range(1, maxCoinsPerGround + 1); // 랜덤한 개수의 코인 생성
+        for (int i = 0; i < coinCount; i++)
+        {
+            float xOffset = Random.Range(-groundWidth / 2, groundWidth / 2);
+            float yOffset = Random.Range(minY, maxY);
+            Vector3 coinPosition = new Vector3(groundPosition.x + xOffset, groundPosition.y + yOffset, 0);
+
+            GameObject coin = Instantiate(coinPrefab, coinPosition, Quaternion.identity, coinParent);
+            coinList.Add(coin);
+        }
+    }
+
+    public void RemoveOldCoins(float playerX)
+    {
+        if (coinList.Count > 0 && coinList[0].transform.position.x < playerX - 10f)
+        {
+            Destroy(coinList[0]);
+            coinList.RemoveAt(0);
+        }
+    }
+}
