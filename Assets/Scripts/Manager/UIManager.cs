5f22fe7aca3c5d891867e15a88494fa8544e0b8f
cuervo314 <gkswjddn30635@naver.com>
2025-02-25 14:44:23 +0900

    feat: UI 애니매이션 추가

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -11,6 +12,15 @@ public class UIManager : MonoBehaviour
     GameUI gameUI;
     PauseUI pauseUI;
     GameOverUI gameOverUI;
+    Canvas canvas;
+    GameObject blackImage;
+    GameObject hpBg;
+    Image image;
+
+    private float pauseSpeed = 3000f;
+    private bool isPauseMoving = false;
+    private RectTransform pauseRect;
+    private float targetY = 0f;
 
     private void Awake()
     {
@@ -19,6 +29,35 @@ public class UIManager : MonoBehaviour
         gameUI = FindFirstObjectByType<GameUI>();
         pauseUI = FindObjectOfType<PauseUI>(true);
         gameOverUI = FindObjectOfType<GameOverUI>(true);
+        canvas = FindFirstObjectByType<Canvas>();
+        blackImage = canvas.transform.GetChild(1).gameObject;
+        hpBg = GameObject.FindGameObjectWithTag("Fill");
+        image = hpBg.GetComponent<Image>();
+
+        pauseRect = pauseUI.GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if(isPauseMoving)
+        {
+            if(pauseRect.anchoredPosition.y > targetY)
+            {
+                float newY = pauseRect.anchoredPosition.y - pauseSpeed * Time.unscaledDeltaTime;
+
+                if (newY <= targetY)
+                {
+                    newY = targetY;
+                    isPauseMoving = true;
+                }
+
+                pauseRect.anchoredPosition = new Vector2(pauseRect.anchoredPosition.x, newY);
+            }
+            else
+            {
+                isPauseMoving = false;
+            }
+        }
     }
 
     public void InGame()//게임 중
@@ -27,12 +66,18 @@ public class UIManager : MonoBehaviour
         gameUI.gameObject.SetActive(true);
         pauseUI.gameObject.SetActive(false);
         gameOverUI.gameObject.SetActive(false);
+        image.color = new Color(114 / 255f, 161 / 255f, 172 / 255f);
     }
 
     public void Pause()//일시정지
     {
         Time.timeScale = 0f;
         pauseUI.gameObject.SetActive(true);
+
+        pauseRect.anchoredPosition = new Vector2(pauseRect.anchoredPosition.x, Screen.height);
+        isPauseMoving = true;
+        blackImage.SetActive(true);
+        image.color = new Color(74 / 255f, 107 / 255f, 114 / 255f);
     }
     
     public void GameOver()//게임 종료
@@ -40,16 +85,21 @@ public class UIManager : MonoBehaviour
         Time.timeScale = 0f;
         gameOverUI.gameObject.SetActive(true);
         pauseUI.gameObject.SetActive(false);
+        blackImage.SetActive(true);
+        image.color = new Color(74 / 255f, 107 / 255f, 114 / 255f);
     }
 
     public void Restart()//게임 재시작
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainScene");
+        image.color = new Color(114 / 255f, 161 / 255f, 172 / 255f);
     }
 
     public void Resume()//게임 재개
     {
         InGame();
+        blackImage.SetActive(false);
+        image.color = new Color(114 / 255f, 161 / 255f, 172 / 255f);
     }
 }
