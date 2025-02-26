18749a029bb002add9ee2c55d7afd89275c0bc39
cuervo314 <gkswjddn30635@naver.com>
2025-02-26 16:27:57 +0900

    feat: 튜토리얼 구현

--- Assets/Scripts/Manager/UIManagerTitle.cs
+++ Assets/Scripts/Manager/UIManagerTitle.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,19 +6,44 @@ using UnityEngine.SceneManagement;
 public class UIManagerTitle : MonoBehaviour
 {
     SettingUI settingUI;
+    GameObject canvas;
+    GameObject tutorial1;
+    GameObject tutorial2;
+    GameObject tutorial3;
 
     private void Awake()
     {
         settingUI = FindObjectOfType<SettingUI>(true);
+        canvas = GameObject.Find("Canvas");
+        tutorial1 = canvas.gameObject.transform.GetChild(9).gameObject;
+        tutorial2 = canvas.gameObject.transform.GetChild(10).gameObject;
+        tutorial3 = canvas.gameObject.transform.GetChild(11).gameObject;
     }
     public void Game()
     {
         SceneManager.LoadScene("MainScene");
     }
 
-    public void Tutorial()
+    public void StartTutorial()
     {
+        tutorial1.SetActive(true);
+    }
 
+    public void NextTutorial1()
+    {
+        tutorial1.SetActive(false);
+        tutorial2.SetActive(true);
+    }
+
+    public void NextTutorial2()
+    {
+        tutorial2.SetActive(false);
+        tutorial3.SetActive(true);
+    }
+
+    public void ExitTutorial()
+    {
+        tutorial3.SetActive(false);
     }
 
     public void Setting()
